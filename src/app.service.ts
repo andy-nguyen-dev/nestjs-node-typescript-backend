a78df7fcb89b037e1beca416d13c598b9da72443
Andre Nguyen <andre_n_nguyen@yahoo.com>
2022-11-07 18:54:33 -0600

    add CORS support -- resolves endpoint response error, when requests are received through an Axios call, from a local reactjs setup.
    I'm so glad I remembered solving this error during my internship 3 years ago lol.
    
    Solution found here (1/2 - backend):
    https://enable-cors.org/server_expressjs.html
    
    And other part of the solution here: (2/2 - frontend):
    https://stackoverflow.com/questions/50949594/axios-having-cors-issue/54713590#54713590

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log(`Request received at localhost:3000`);
     return 'It works sonn lol';
   }
 }
